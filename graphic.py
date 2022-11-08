82253baad1f4b709cfad90d522197175e5eaeb01
skolton09 <skolton09@gmail.com>
2022-11-08 01:14:08 -0700

    Removed pause and play buttons, added dropdown menu for playlists

--- graphic.py
+++ graphic.py
@@ -6,10 +6,14 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import *
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.slider import Slider
 from playlists import *
 from get_id import *
 
-spotify = "temp"
+spotify = None
+playlist_name = None
 
 class WockaShuffle(App):
 
@@ -19,28 +23,8 @@ class WockaShuffle(App):
 
     def build(self):
         
-        def pause(instance):
-            try:
-                self.spotify.pause_playback()
-            except spotipy.exceptions.SpotifyException as e:
-                error_code = e.http_status
-                if error_code == 404:
-                    print("No Active Device")
-                if error_code == 403:
-                    print("Already Paused")
-
-        def start(instance):
-            try:
-                self.spotify.start_playback()
-            except spotipy.exceptions.SpotifyException as e:
-                error_code = e.http_status
-                if error_code == 404:
-                    print("No Active Device")
-                if error_code == 403:
-                    print("Already Playing")
-
         def shuffle(instance):
-            playlist_name = get_id("PLAYLIST_NAME")
+            playlist_name = self.playlist_name
             cur_user = self.spotify.current_user()["id"]
             print(cur_user)
             user_playlists = self.spotify.current_user_playlists()["items"]
@@ -57,22 +41,32 @@ class WockaShuffle(App):
             shuffled_track_list = shuffle_tracks(categorized_tracks, playlist_length)
             shuffled_playlist = create_playlist(self.spotify, cur_user, shuffled_track_list, user_playlists, playlist_length)
             
-            
+        def button_selected(button):
+            self.playlist_dropdown.select(button.text)
+            self.playlist_name = button.text
             
         layout = FloatLayout(size_hint=(1, 1))
-        # # Bottom Left
-        # pause_button = Button(text="Pause", pos_hint={"x":0, "y":0}, size_hint=(.5, .5))
-        # pause_button.bind(on_press=pause)
-        # # Bottom Right
-        # start_button = Button(text="Play", pos_hint={"x":0.5, "y":0}, size_hint=(.5, .5))
-        # start_button.bind(on_press=start)
-        # Top Left
-        shuffle_button = Button(text="Shuffle", pos_hint={"x":0.25, "y":0.25}, size_hint=(.5, .5))
-        shuffle_button.bind(on_press=shuffle)
+        
+        self.playlist_dropdown = DropDown()
+        # Loop through playlists adding to dropdown
+        for i in self.spotify.current_user_playlists()["items"]:
+            name = i["name"]
+            self.btn = Button(text=f"{name}", height=44, size_hint_y=None)
+            
+            # With every new button, add a dropdown select function
+            self.btn.bind(on_release=button_selected)
+            self.playlist_dropdown.add_widget(self.btn)
 
-        # layout.add_widget(pause_button)
-        # layout.add_widget(start_button)
-        layout.add_widget(shuffle_button)
+        self.dropdown_button = Button(text="Select a playlist", pos_hint={"x":0.25, "y":0.8},size_hint=(.5, None))
+        self.dropdown_button.bind(on_release=self.playlist_dropdown.open)
+        self.playlist_dropdown.bind(on_select=lambda instance, x: setattr(self.dropdown_button, 'text', x))
+
+        self.shuffle_button = Button(text="Shuffle", pos_hint={"x":0.25, "y":0}, size_hint=(.5, .5))
+        self.shuffle_button.bind(on_press=shuffle)
+
+
+        layout.add_widget(self.shuffle_button)
+        layout.add_widget(self.dropdown_button)
         return layout
     
    
--- playlists.py
+++ playlists.py
@@ -2,7 +2,7 @@ import random
 
 def get_playlist(user_playlists, to_get):
     for playlist in user_playlists:
-        if playlist["name"] == to_get:
+        if playlist["name"].lower() == to_get.lower():
             return playlist
     return "Error: Could not find playlist"
 
