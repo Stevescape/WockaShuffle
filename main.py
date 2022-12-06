d73ad6b97d31605976a98879bcc8b58d50204670
skolton09 <skolton09@gmail.com>
2022-12-06 01:10:54 -0700

    Did things

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 from playlists import *
 from graphic import WockaShuffle  
 from kivy.app import App 
 
 def main():
     # get_ids returns: [client_id, client_secret]
-    if __name__ == "__main__":
-        CLIENT_ID = "f59708637b0f46958f3a5ad18f926a46"
-        CLIENT_SECRET = "068d885a0dc3448291303ffb49cbfd05"
-        REDIRECT_URI = "http://localhost:8080/callback/"
-        auth = SpotifyOAuth(client_id = CLIENT_ID, 
-                            client_secret = CLIENT_SECRET,
-                            redirect_uri = REDIRECT_URI,
-                            scope = "user-library-read, playlist-read-private, \
-                                user-modify-playback-state, user-library-modify,\
-                                     playlist-modify-private, playlist-read-collaborative")
-        spotify = spotipy.Spotify(auth_manager=auth)
+    CLIENT_ID = "f59708637b0f46958f3a5ad18f926a46"
+    CLIENT_SECRET = "cbfe0157f7374d4aa11d2e66176bba4a"
+    REDIRECT_URI = "http://localhost:8080/callback/"
+    auth = SpotifyOAuth(client_id = CLIENT_ID, 
+                        client_secret = CLIENT_SECRET,
+                        redirect_uri = REDIRECT_URI,
+                        cache_path = "../cache.txt",
+                        scope = "user-library-read, playlist-read-private, \
+                            user-modify-playback-state, user-library-modify,\
+                                    playlist-modify-private, playlist-read-collaborative")
+    token = auth.get_cached_token()
+    spotify = spotipy.Spotify(oauth_manager=auth)
 
-        WockaShuffle(spotify).run()
+    WockaShuffle(spotify).run()
 
         
 main()
