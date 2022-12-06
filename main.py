bbe3ba64f9ff4fc215350827f3eb549bfbc8956c
skolton09 <skolton09@gmail.com>
2022-12-05 23:36:57 -0700

    Removed get id

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from get_id import get_id
 from playlists import *
 from graphic import WockaShuffle  
 from kivy.app import App 
@@ -8,9 +7,9 @@ from kivy.app import App
 def main():
     # get_ids returns: [client_id, client_secret]
     if __name__ == "__main__":
-        CLIENT_ID = get_id("CLIENT_ID")
+        CLIENT_ID = "f59708637b0f46958f3a5ad18f926a46"
         CLIENT_SECRET = "068d885a0dc3448291303ffb49cbfd05"
-        REDIRECT_URI = get_id("REDIRECT_URI")
+        REDIRECT_URI = "http://localhost:8080/callback/"
         auth = SpotifyOAuth(client_id = CLIENT_ID, 
                             client_secret = CLIENT_SECRET,
                             redirect_uri = REDIRECT_URI,
